9d762b39848ae02297234bc4278eece953c86564
Gwendolyn Margulies <gam330@lehigh.edu>
2026-09-09 15:21:49 -0400

    Add files via upload

---
+++ Programming Activities/Program3.py
@@ -0,0 +1,7 @@
+# Revision of Program 2 on September 9
+# saving the name as a string in variable "name"
+
+name= "Gwendolyn"
+
+# output the contents of the variable "name" to the REPL
+print(name) 
